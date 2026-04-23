6f7d109318885f7e073eec7ae85cd6b05babe3e7
Nick <167111495+Prithc45@users.noreply.github.com>
2026-04-23 19:16:49 +0530

    Add script to clone Xiaomi device repositories

---
+++ Clone_repos.sh
@@ -0,0 +1,11 @@
+git clone https://github.com/Prithc45/android_device_xiaomi_sm6125-common.git device/xiaomi/sm6125-common
+
+git clone https://github.com/Prithc45/android_device_xiaomi_ginkgo.git device/xiaomi/ginkgo
+
+git clone https://github.com/Prithc45/android_kernel_xiaomi_sm6125.git kernel/xiaomi/sm6125
+
+git clone https://github.com/Prithc45/proprietary_vendor_xiaomi_sm6125-common.git vendor/xiaomi/sm6125-common
+
+git clone https://github.com/Prithc45/proprietary_vendor_xiaomi_ginkgo.git vendor/xiaomi/ginkgo
+
+git clone https://github.com/Prithc45/android_hardware_xiaomi.git hardware/xiaomi
